96492c350a7f67d152242c5b568d948405cbfc80
ankithbti <ankithbti007@gmail.com>
2014-06-14 02:43:02 +0530

    Updating the font and Team page for ElanTattva as suggested by Suresh on 14 Jun 2014

--- ourteam.php
+++ ourteam.php
@@ -26,6 +26,8 @@
 		      <li class="active"><a href="#suresh"><i class="icon-chevron-right"></i> <i class="icon-user"></i> Suresh Sivaraman</a></li>
 		      <li><a href="#archana"><i class="icon-chevron-right"></i> <i class="icon-user"></i> Archana Krishnamurthy</a></li>
 		      <li><a href="#raphael"><i class="icon-chevron-right"></i> <i class="icon-user"></i> Raphael Joseph</a></li>
+		      <li><a href="#prarthana"><i class="icon-chevron-right"></i> <i class="icon-user"></i> Prarthana Prateek Kaul</a></li>
+		      <li><a href="#xavier"><i class="icon-chevron-right"></i> <i class="icon-user"></i> Xavier J Stalin</a></li>
 		      <li><a href="#kiran"><i class="icon-chevron-right"></i> <i class="icon-user"></i> Kiran Shetty</a></li>
 		      <li><a href="#kabir"><i class="icon-chevron-right"></i> <i class="icon-user"></i> Kabir Ahluwalia</a></li>
 		    </ul>
@@ -38,16 +40,16 @@
 	        ================================================== -->
 	        <section id="suresh">
 	          <div class="page-header">
-	            <h1>Suresh Sivaraman<span style='font-size: 18px;'> ( Executive Coach &amp; Trainer )</span></h1>
+	            <h1>Suresh Sivaraman<span style='font-size: 18px;'> ( Founder )</span></h1>
 	          </div>
 	          
 		      <img src="images/suresh.png" class="img-rounded img-polaroid" style="margin:10px;width:200px;height:150px;position:relative;float:left;"/>    
 	          <!-- <p><strong>Executive Coach &amp; Trainer</strong></p> -->
-	          <p><strong>&raquo;</strong> Over 16 years of experience in corporate life.</p>
-	          <p><strong>&raquo;</strong> Certified Coach (Neuro Leadership Group, and ANLP India)</p>
-	          <p><strong>&raquo;</strong> Co-Founder of Elan Tattva, which is into personal excellence training &amp; coaching.</p>
-	          <p><strong>&raquo;</strong> MBA (PGSEM) from IIM Bangalore, Class of 2006</p>
-	          <p><strong>&raquo;</strong> Delivery Manager in Wipro Technologies till 2011</p>
+	          <p><strong>&raquo;</strong> MBA (PGSEM) from IIM Bangalore.</p>
+	          <p><strong>&raquo;</strong> Worked as Delivery Manager in Wipro Technologie.</p>
+	          <p><strong>&raquo;</strong> Certified Coach from Neuro Leadership Group.</p>
+	          <p><strong>&raquo;</strong> Certified NLP Trainer &amp; Life Coach (ANLP)</p>
+	          <p><strong>&raquo;</strong> Over 16 years of corporate experience.</p>
 	          <p><strong>&raquo;</strong> Extensive work experience in India and Europe.</p>
 	        </section>
 
@@ -60,11 +62,13 @@
 	          
 		      <img src="images/archana.png" class="img-rounded img-polaroid" style="margin:10px;width:200px;height:150px;position:relative;float:left;"/>    
 	          <!-- <p><strong>Executive Coach &amp; Trainer</strong></p> -->
+	          <p><strong>&raquo;</strong> Ex Regional Director, ASSOCHAM.</p>
+	          <p><strong>&raquo;</strong> Is certified by Franklin Covey Institute, Australia as Leadership Coach.</p>
+	          <p><strong>&raquo;</strong> Works closely with Governments and Trade &amp; Industry Associations in various senior positions.</p>
 	          <p><strong>&raquo;</strong> Over 22 years of experience in corporate life.</p>
 	          <p><strong>&raquo;</strong> Has been an entrepreneur for over 12 years.</p>
-	          <p><strong>&raquo;</strong> Is certified by Franklin Covey Institute, Australia as Leadership Coach.</p>
 	          <p><strong>&raquo;</strong> Is Founder Director of CorNet PR - which is into Corporate Networking and servicing M&amp;E segment.</p>
-	          <p><strong>&raquo;</strong> Works closely with Governments and Trade & Industry Associations in various senior positions.</p>
+	          
 	        </section>
 
 	        <!-- 
@@ -78,11 +82,53 @@
 	          <!-- <p><strong>Executive Coach &amp; Trainer</strong></p> -->
 	          <p><strong>&raquo;</strong> Corporate Trainer in Leadership, Behavioural &amp; Sales with varied industrial experience.</p>
 	          <p><strong>&raquo;</strong> Organizes open workshops with Times of India group.</p>
-	          <p><strong>&raquo;</strong> Asst. Team lead in Technicolor India from (Jan 2006 – Oct 2012); was picked as the star performer of gaming unit 2011 – 2012</p>
+	          <p><strong>&raquo;</strong> Previously worked as Asst. Team lead in Technicolor India; star performer of gaming unit.</p>
 	          <p><strong>&raquo;</strong> Guest Lecturer and Mentor for ‘Arena Multimedia’ and ‘Image Multimedia’</p>
 	          <p><strong>&raquo;</strong> Actively partnered with YMCA in organizing youth camps, and drug de-addiction camps.</p>
 	          <p><strong>&raquo;</strong> Organized youth empowerment programs.</p>
 	          <p><strong>&raquo;</strong> Rich philosophical and psychological back ground.</p>
+	        </section>
+
+	        <!-- 
+	        ================================================== -->
+	        <section id="prarthana">
+	          <div class="page-header">
+	            <h1>Prarthana Prateek Kaul<span style='font-size: 18px;'> ( Visionary Social Entrepreneur, Trainer &amp; Consultant )</span></h1>
+	          </div>
+	          
+		      <img src="images/prarthana.png" class="img-rounded img-polaroid" style="margin:10px;width:200px;height:150px;position:relative;float:left;"/>    
+	          <!-- <p><strong>Executive Coach &amp; Trainer</strong></p> -->
+	          <p><strong>&raquo;</strong> Guest Speaker at different institutes including IIT Chennai, IIMB, NMIMS, TISS.</p>
+	          <p><strong>&raquo;</strong> Visionary Social Entrepreneur: founder of Giftabled (an inclusive gift store)</p>
+	          <p><strong>&raquo;</strong> Consultant and Trainer – Rediscover your passion, Volunteering, Disability Awareness.</p>
+	          <p><strong>&raquo;</strong> 10 years of extensive NGO &amp; Volunteering  Experience in India:</p>
+	          
+	          <p><strong>&nbsp;&nbsp;&nbsp;&nbsp;&#8226;</strong> Regional Head – East and South at iVolunteer for 8 years.</p>
+	          <p><strong>&nbsp;&nbsp;&nbsp;&nbsp;&#8226;</strong> On Board of Om Ashram, an Old Age Home in Bangalore.</p>
+	          <p><strong>&nbsp;&nbsp;&nbsp;&nbsp;&#8226;</strong> Trained in using “Basic Sign language” and “Basic Uncontracted Braille”</p>
+
+
+	          <p><strong>&raquo;</strong> PGDBA (Finance) from Symbiosys Centre for Distance Learning.</p>
+	          <p><strong>&raquo;</strong> Worked as Sr. Procurement Specialist in IBM.</p>
+	        </section>
+
+	        <!-- 
+	        ================================================== -->
+	        <section id="xavier">
+	          <div class="page-header">
+	            <h1>Xavier J Stalin<span style='font-size: 18px;'> ( Senior Corporate Training Consultant &amp; Trainer )</span></h1>
+	          </div>
+	          
+		      <img src="images/xavier.png" class="img-rounded img-polaroid" style="margin:10px;width:200px;height:150px;position:relative;float:left;"/>    
+	          <!-- <p><strong>Executive Coach &amp; Trainer</strong></p> -->
+	          <p><strong>&raquo;</strong> “General Management &amp; Entrepreneurship” from IIM-B.</p>
+	          <p><strong>&raquo;</strong> “Leadership &amp; Personal Excellence” using NLP techniques from St. Joseph’s College.</p>
+	          <p><strong>&raquo;</strong> “Training Need Analysis &amp; Risk Audit” from J.P. Morgan.</p>
+	          <p><strong>&raquo;</strong> “Facilitator Development” from Hero Mind Mine.</p>
+	          <p><strong>&raquo;</strong> Over 7 years of experience in Behavioral &amp; Corporate training.</p>
+	          <p><strong>&raquo;</strong> Worked as an MBA Visiting Faculty in many reputed colleges.</p>
+	          <p><strong>&raquo;</strong> Counseled over 500 people which includes students, employees, doctors &amp; marries couples.</p>
+	          <p><strong>&raquo;</strong> Have been a Corporate Trainer in many reputed companies.</p>
 	        </section>
 
 	        <!--
